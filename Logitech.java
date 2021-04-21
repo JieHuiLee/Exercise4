53d99b104201d32f682a9c4e6adfff25963a6d24
JieHuiLee <81312864+JieHuiLee@users.noreply.github.com>
2021-04-21 22:50:33 +0800

    Update Logitech.java

--- Logitech.java
+++ Logitech.java
@@ -4,7 +4,7 @@ public class Logitech extends Mouse{
 		
 	private String wireless;//wl
 	private int batteryLife;//l
-    private double payment;//py
+        private double payment;//py
 	
 	public Logitech(String m, String c, String b,double w, double p, int q,String wl,int l,double py) {
 		super(m,c,b,w,p,q);
@@ -19,18 +19,18 @@ public class Logitech extends Mouse{
 	
 	 public int getBatteryLife() {
 	    return batteryLife;
-	  }
+	 }
 	  
 	 public double getPayment() {
 		return payment;
-		}
+	}
 		
 	public double balance() {
 		return payment - super.calDiscount() ;
-		}
+	}
 		
 	public String toString() {
 		return super.toString()+ "\nWireless\t: " + wireless + "\nBattery Life\t: " + batteryLife +"Days" + "\nPayment\t   : RM " + payment ;
-	  }		
+	}		
 	
 }
