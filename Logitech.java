7f87e381b848a687f4ce85b589ac9bf07822fd22
JieHuiLee <81312864+JieHuiLee@users.noreply.github.com>
2021-04-21 22:46:20 +0800

    Create Logitech.java

---
+++ Logitech.java
@@ -0,0 +1,36 @@
+package Inheritance_Exercise4;
+
+public class Logitech extends Mouse{
+		
+	private String wireless;//wl
+	private int batteryLife;//l
+    private double payment;//py
+	
+	public Logitech(String m, String c, String b,double w, double p, int q,String wl,int l,double py) {
+		super(m,c,b,w,p,q);
+		wireless = wl;
+		batteryLife = l;
+		payment = py;
+		}
+	
+	public String getWireless() {
+		return wireless;
+	}
+	
+	 public int getBatteryLife() {
+	    return batteryLife;
+	  }
+	  
+	 public double getPayment() {
+		return payment;
+		}
+		
+	public double balance() {
+		return payment - super.calDiscount() ;
+		}
+		
+	public String toString() {
+		return super.toString()+ "\nWireless\t: " + wireless + "\nBattery Life\t: " + batteryLife +"Days" + "\nPayment\t   : RM " + payment ;
+	  }		
+	
+}
